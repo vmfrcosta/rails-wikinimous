e241152ce9b499608f7a05bb8f491de63aa7dc1b
vinicius <vmfrcosta@gmail.com>
2019-11-12 18:09:05 -0300

    Coded index page

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+  get 'articles', to: 'articles#index'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :article
 end
